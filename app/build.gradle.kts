87a55690aee8bdfa2d4b627334fde194e328c2dc
Atri Hegde <iamatrihegde@outlook.com>
2023-08-18 11:48:24 +0100

    feat: integrated supabase sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,13 @@ android {
 }
 
 dependencies {
+    // Supabase dependencies
+    implementation(platform("io.github.jan-tennert.supabase:bom:1.2.0"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:realtime-kt")
+    implementation("io.ktor:ktor-client-okhttp:2.3.3")
 
+    // Default dependencies
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
